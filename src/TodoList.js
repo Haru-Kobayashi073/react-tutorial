ea49251726bcde4c0f1c3894eced56435427d6f8
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-01-19 12:20:40 +0900

    useRefとonClickでTodoのリストを追加

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Todo from "./Todo";
 
 const TodoList = ({ todos }) => {
   return (
-    <div>{todos}</div>
+    todos.map((todo) => <Todo todo={todo} key={todo.id} />)
   )
 }
 
